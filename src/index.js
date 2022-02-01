c2bf909a2c3e407ac55d01a86aefe731e0dfb975
enigma <camilo.murcia.pimentel@gmail.com>
2022-02-01 09:47:10 -0500

    chore: added express server setting

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Hello')
+import express from "express";
+
+const PORT = 3000;
+const app = express(PORT);
+app.listen(PORT);
+
+console.log(`Server listening on port http://localhost:${PORT}`);
