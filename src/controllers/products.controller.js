12b6c3fb74269d679cc5379371b79a3d405ac19f
enigma <camilo.murcia.pimentel@gmail.com>
2022-02-02 14:27:18 -0500

    chore:read products by id from the database

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,6 +1,5 @@
 import Product from '../models/Product';
 
-
 export const createProduct = async(req, res) => {
     const {name,category,price,imgUrl} = req.body;
     const newProduct = new Product({name,category,price,imgUrl});
@@ -12,6 +11,11 @@ export const getProducts = async(req, res) => {
     const products = await Product.find();
     res.status(201).json(products);
 }
-export const getProductById = (req, res) => {}
+
+export const getProductById = async(req, res) => {
+    const product = await Product.findById(req.params.productId);
+    res.status(200).json(product);
+}
+
 export const updateProductById = (req, res) => {}
 export const deleteProductById = (req, res) => {}
