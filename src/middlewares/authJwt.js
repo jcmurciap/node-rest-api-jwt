d94016d0ef9cb67b1c49c1e7b3ebb2491b787e1a
enigma <camilo.murcia.pimentel@gmail.com>
2022-02-03 16:40:59 -0500

    chore:add token validation for products router

---
+++ src/middlewares/authJwt.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import config from '../config'
+import User from "../models/User";
+
+export const verifyToken = async(req,res,next) => {
+    
+    try {
+        const token = req.headers["x-access-token"];
+        console.log(token);
+        if(!token) return res.status(403).json({message: "No token provided"});
+        
+        const decoded = jwt.verify(token, config.SECRET)
+        req.userId = decoded.id;
+    
+        const user = await User.findById(req.userId, {password: 0});
+        if(!user) return res.status(404).json({message: 'No user found'});
+        
+        next();
+    } catch (error) {
+        return res.status(401).json({message: "Unauthorized"});
+    }
+}
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,12 +1,13 @@
 import {Router} from 'express';
 import * as productsController from '../controllers/products.controller';
+import { verifyToken } from '../middlewares';
 
 const router = Router();
 
-router.post('/', productsController.createProduct);
+router.post('/', verifyToken, productsController.createProduct);
 router.get('/', productsController.getProducts);
 router.get('/:productId', productsController.getProductById);
-router.put('/:productId', productsController.updateProductById);
-router.delete('/:productId', productsController.deleteProductById);
+router.put('/:productId', verifyToken, productsController.updateProductById);
+router.delete('/:productId', verifyToken, productsController.deleteProductById);
 
 export default router;
