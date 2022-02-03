dfaaee98dfed65fe8934c931ce286e1c714aee08
enigma <camilo.murcia.pimentel@gmail.com>
2022-02-03 11:44:05 -0500

    chore:add json web token validation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
+import jwt from 'jsonwebtoken';
 import User from '../models/User';
+import config from "../config";
 
 export const signUp = async (req, res) => {
     const {username,email,password,roles} = req.body;
@@ -8,8 +10,12 @@ export const signUp = async (req, res) => {
         email,
         password: User.encryptPassword(password)
     })
-    await newUser.save();
-    res.json('signUp');
+    const savedUser = await newUser.save();
+    const token = jwt.sign({id: savedUser._id}, config.SECRET, {
+        expiresIn: 86400
+    });
+
+    res.status(200).json(token);
 }
 
 export const signIn = async (req, res) => {
