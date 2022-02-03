9a019c7727ba880645894228ed88737290c790f6
enigma <camilo.murcia.pimentel@gmail.com>
2022-02-03 15:45:01 -0500

    chore: set roles and token to active users at log-in

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -30,5 +30,15 @@ export const signUp = async (req, res) => {
 }
 
 export const signIn = async (req, res) => {
-    res.json('signIn');
+    const userFound = await User.findOne({email: req.body.email}).populate("roles");
+    if(!userFound) return res.status(400).json({message: "User not found"})
+    
+    const matchPassword = await User.comparePassword(req.body.password, userFound.password)
+    if(!matchPassword) return res.status(401).json({message:"Password incorrect", token:null});
+
+    const token = jwt.sign({id:userFound._id}, config.SECRET, {
+        expiresIn: 86400
+    })
+
+    res.json({token});
 }
