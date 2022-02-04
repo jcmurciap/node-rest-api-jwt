14fa0b63d8f9027fe30efa29ec29f2f36c22a4cd
enigma <camilo.murcia.pimentel@gmail.com>
2022-02-04 10:59:28 -0500

    chore: asociate authentication roles to product routes

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,2 +1,2 @@
-import { verifyToken } from "./authJwt";
-export {verifyToken}
+import * as authJwt  from "./authJwt";
+export {authJwt}
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,13 +1,13 @@
 import {Router} from 'express';
 import * as productsController from '../controllers/products.controller';
-import { verifyToken } from '../middlewares';
+import { authJwt } from '../middlewares';
 
 const router = Router();
 
-router.post('/', verifyToken, productsController.createProduct);
+router.post('/', [authJwt.verifyToken, authJwt.isModerator], productsController.createProduct);
 router.get('/', productsController.getProducts);
 router.get('/:productId', productsController.getProductById);
-router.put('/:productId', verifyToken, productsController.updateProductById);
-router.delete('/:productId', verifyToken, productsController.deleteProductById);
+router.put('/:productId', [authJwt.verifyToken, authJwt.isAdmin], productsController.updateProductById);
+router.delete('/:productId', [authJwt.verifyToken, authJwt.isAdmin], productsController.deleteProductById);
 
 export default router;
